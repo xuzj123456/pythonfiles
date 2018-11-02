871c3dfc7bb50d5735aa80f4d0f32db7d05d9c18
Xu <13507568923@163.com>
2018-11-02 16:15:22 +0800

    .

---
+++ learning/pandas learning.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import pandas as pd
+
+d = {'one' : pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+      'two' : pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])}
+df = pd.DataFrame(d)
+
+df['three']=pd.Series([10,20,30],index=['a','b','c'])
+df['four']=df['one']+df['three']
+
+print(df['one'])        # 列选择
+
+print(df[1:2])
+print(df.loc['a'])
+print(df.iloc[1])       # 行选择
