cb3da8100eba72adb20add6ef4d71195a987144f
Xu <13507568923@163.com>
2018-11-26 17:17:20 +0800

    .

--- macroeco/hw.py
+++ macroeco/hw.py
@@ -32,24 +32,34 @@ class Solution:
 
 
     @staticmethod
-    def forward(n=200):
+    def forward(c_0, n=200):
         c_bar = 1
-        for c_0 in np.linspace(c_bar, c_max, 10000):
-            c = np.zeros((n, 1))
-            k = np.zeros((n, 1))
-            c[0] = c_0
-            k[0] = k_0
-            try:
-                for i in range(n-1):
-                    k[i+1] = k[i]**alpha+(1-delta)*k[i]-c[i]
-                    c[i+1] = (c[i]-c_bar)*beta*((1-delta)+alpha*k[i+1]**(alpha-1))+c_bar
-                    if k[i+1] <= 0:
-                        raise Exception
-                if abs(k[-1]-k_ss) <= 0.01 and abs(c[-1]-c_ss) <= 0.01:
-                    return k, c
-            except:
-                pass
+        c = np.zeros((n, 1))
+        k = np.zeros((n, 1))
+        c[0] = c_0
+        k[0] = k_0
+        for i in range(n-1):
+            k[i+1] = k[i]**alpha+(1-delta)*k[i]-c[i]
+            c[i+1] = (c[i]-c_bar)*beta*((1-delta)+alpha*k[i+1]**(alpha-1))+c_bar
+            if k[i+1]<=0:
+                k[i+1]=0
+                break
         return k, c
+
+    @staticmethod
+    def bisection():
+        c_bar = 1
+        c0_min = c_bar
+        c0_max = c_max
+        while c0_max-c0_min>10**(-10):
+            c0=(c0_min+c0_max)/2
+            k_simu, c_simu = Solution.forward(c0)
+            if k_simu[-1] <= k_ss:
+                c0_max = c0
+            else:
+                c0_min = c0
+        return k_simu, c_simu
+
 
 
     @staticmethod
@@ -70,5 +80,5 @@ if __name__ == '__main__':
     s = Solution
     k_back, c_back = s.backward()
     s.plot(k_back, c_back, 'Backward method')
-    k_forward, c_forward = s.forward()
+    k_forward, c_forward = s.bisection()
     s.plot(k_forward, c_forward, 'Forward method')
